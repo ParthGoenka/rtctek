25ba51e33ba23b387bda0fe0713a7d2ffa6561e8
Parth Goenka <parth.goenka@rtctek.global>
2025-02-20 15:52:00 +0530

    day9

--- React/practice/redux/user/src/components/user.jsx
+++ React/practice/redux/user/src/components/user.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect} from "react";
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {fetchData} from "../index"
 
@@ -11,23 +11,22 @@ function User() {
           }
           if (!Array.isArray(users)) {
             return( "No users found or invalid data format")
-         }else{
+         }
         
           return (
             <div>
               <h1>Users</h1>
               {errormsg && <p>{errormsg}</p>}
-              {console.log(users)}
-              <ul>
-                {users.map((user, index) => (
-                  <li key={index}><h3>{user.username}&nbsp;-&nbsp;{user.name}</h3></li> 
+              <ol>
+                {users.map((user) => (
+                  <li key={user.id}><h3>{user.username}&nbsp;-&nbsp;{user.name}</h3></li> 
                 ))}
-              </ul>
+              </ol>
               <button onClick={handle}>Get Users</button>
               
             </div>
           );}
-        }
+        
         
 
 export default User;
